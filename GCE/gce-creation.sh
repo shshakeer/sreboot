37ec796ec861ef89a4c8fc03b9350d2714060bf7
root <root@localhost.localdomain>
2021-08-20 04:55:06 -0700

    gce scripts added

---
+++ GCE/gce-creation.sh
@@ -0,0 +1,6 @@
+echo "##########Creating compute instance###########"
+echo "please enter name of the GCE VM: "
+read gcevm
+
+
+gcloud compute instances create $gcevm --project=sreboot --zone=us-west4-b --machine-type=e2-medium --network-interface=network-tier=PREMIUM,subnet=default --metadata=enable-oslogin=TRUE,startup-script=yum\ update\ -y$'\n'yum\ install\ git\ -y$'\n'yum\ install\ docker\ -y$'\n'systemctl\ start\ docker$'\n'yum\ install\ wget\ unzip\ -y$'\n'wget\ https://releases.hashicorp.com/terraform/0.12.2/terraform_0.12.2_linux_amd64.zip$'\n'mv\ terraform_0.12.2_linux_amd64.zip\ /usr/local/bin/$'\n'cd\ /usr/local/bin/$'\n'unzip\ terraform_0.12.2_linux_amd64.zip$'\n'git\ clone\ https://github.com/shakeerdcm/sreboot.git$'\n'cd\ sreboot/NGINXGCR$'\n'sh\ install.sh$'\n'docker\ run\ -d\ -p\ 80:80\ gcr.io/sreboot/my-web-server:v1 --maintenance-policy=MIGRATE --service-account=912494784009-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server,https-server --image=centos-7-v20210817 --image-project=centos-cloud --boot-disk-size=20GB --boot-disk-type=pd-balanced --boot-disk-device-name=instance-1 --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any
