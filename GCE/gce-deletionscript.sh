817bdce983f677a3b9775f6b3407c9c4a1045512
root <root@localhost.localdomain>
2021-08-24 04:07:02 -0700

    Added project variable

--- GCE/gce-deletionscript.sh
+++ GCE/gce-deletionscript.sh
@@ -1,7 +1,9 @@
 echo "Warning: You have executed deletion script to delete gce instance"
 echo "Please enter name of gce instance: "
 read gce
-echo "Please type to delete to destroy this instance: ", $gce
+echo "Please Enter name of the project: "
+read proj
+echo "Please type to delete to destroy instance: ", $gce, "in project", $proj
 
 read confirm
 
@@ -9,7 +11,7 @@ if [ $confirm = delete ]
 then
 echo "executing deletion script"
 
-gcloud compute instances delete $gce --project=sreboot
+gcloud compute instances delete $gce --project="$proj"
 
 else
 echo "User not confirmed on deletion of gce"
