b181be2438a8370cf8e7eea475099d5388a45fa1
root <root@localhost.localdomain>
2021-08-25 05:35:32 -0700

    Added varibale file

---
+++ TERRAFORM/GKECluster/vars.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+    type = string
+}
+
+variable "region" {
+    type = string
+}
+
+variable "zone" {
+    type = string
+}
+
+variable "cred_file" {
+    type = string
+}
+
+variable "network_name" {
+    type = string
+}
